43f7ed10c612acb0f342b237669fe5c99f7a23eb
MohammadMahdi Taheri <mmtaheri@yandex.com>
2022-05-20 10:40:08 +0400

    + present task in task component + scoped styles + emit delete task event

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,5 +1,12 @@
 <template>
-  <h3>{{task.text}}</h3>
+  <div 
+    :class="[task.reminder ? 'reminder' : '', 'task']">
+    <h3>
+      {{task.text}}
+      <i @click="$emit('deleteTask', task.id)" class="fas fa-times"></i>  
+    </h3>
+    <p>{{task.time}}</p>
+</div>
 </template>
 
 <script>
@@ -10,3 +17,23 @@
     }
   }
 </script>
+
+<style scoped>
+  .fas {
+  color: red;
+  }
+  .task {
+    background: #e9e9e9;
+    margin: 5px;
+    padding: 10px 20px;
+    cursor: pointer;
+  }
+  .task.reminder {
+    border-left: 5px solid green;
+  }
+  .task h3 {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+</style>
