920c09acc6f26a98bc1b8a28282d47247d3e53b4
MohammadMahdi Taheri <mmtaheri@yandex.com>
2022-05-21 17:22:59 +0400

    + Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+    <p>Mohammad Mahdi Taheri &copy; 2022 </p>
+    <a href="/about">About us</a>
+  </footer>
+</template>
+
+<style scoped>
+  a {
+    color: #333;
+  }
+
+  footer {
+    margin-top: 30px;
+    text-align: center;
+    font-size: 0.9rem;
+  }
+</style>
