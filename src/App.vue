cde8cddf8234da2607d4577acd87c1009e672c9f
MohammadMahdi Taheri <mmtaheri@yandex.com>
2022-05-21 17:11:12 +0400

    + (U and D of crud) -+ refactor onDelete to persist delete on db -+ refactor toggleReminder to persist update on db

--- src/App.vue
+++ src/App.vue
@@ -30,21 +30,40 @@ export default {
     }
   },
   methods: {
-    onDelete(id) {
+    async onDelete(id) {
       if (confirm('Are you sure about deleting the task?')) {
-        this.tasks = this.tasks.filter((task) => task.id !== id)
+        const res = await fetch(`api/tasks/${id}`, {
+          method: 'DELETE'
+        })
+
+        res.status === 200 
+          ? (this.tasks = this.tasks.filter((task) => task.id !== id))
+          : alert('something wrong happened! :(')
       }
     },
-    toggleReminder(id) {
+    async toggleReminder(id) {
+      const theTask = await this.fetchTask(id);
+      const updatedTask = {...theTask, reminder: ! theTask.reminder};
+
+      const res = await fetch(`api/tasks/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(updatedTask)
+      });
+      
+      const data = await res.json();
+
       this.tasks = this.tasks.map((task) => task.id === id 
-      ? {...task, reminder: ! task.reminder} 
+      ? {...task, reminder: data.reminder} 
       : {...task});
     },
     async addTask(newTask) {
       const res = await fetch('api/tasks', {
         method: 'POST',
         headers: {
-          'Content-type': 'application/json',
+          'Content-type': 'application/json'
         },
         body: JSON.stringify(newTask)
       });
