63f0c4951169511b849e5650fe76758361b08d67
MohammadMahdi Taheri <mmtaheri@yandex.com>
2022-05-21 18:57:07 +0400

    + About view

---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>This is all about learning vue js</h3>
+  <h4>Version 1.0.0</h4>
+  <router-link to="/">Go Back</router-link>
+</template>
+
+<script>
+  export default {
+    props: {
+      mode: Boolean
+    }
+  }
+</script>
